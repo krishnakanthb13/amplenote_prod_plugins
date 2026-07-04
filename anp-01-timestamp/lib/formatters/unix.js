6039953cf3a2ffee8e20f3ac8398cedb18e53a0f
Krishna Kanth B <75969083+krishnakanthb13@users.noreply.github.com>
2026-07-04 12:16:14 +0530

    v0.0.19 - Removed to add sub-modules

--- anp-01-timestamp/lib/formatters/unix.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns the current Unix timestamp.
- * @param {object} app - The Amplenote app object.
- * @returns {string} - The current Unix timestamp as a string.
- */
-export function unix(app) {
-    return Math.floor(Date.now() / 1000).toString();
-}
